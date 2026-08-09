9915e428d4f3c08fcddbf65c2955e4e7c0863390
Tarq Asere <tarqasere@gmail.com>
2026-08-09 14:38:21 +0300

    Style registration confirmation page

--- process_registration.php
+++ process_registration.php
@@ -1,3 +1,8 @@
+<?php include "header.php"; ?>
+
+<main>
+    <div class="register-box">
+
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -9,28 +14,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($name == "" || $id == "" || $email == "" || $event == "") {
 
-        echo "Please fill in all fields.";
+        echo "<h2>Registration Error</h2>";
+        echo "<p>Please fill in all fields.</p>";
+        echo "<a class='btn' href='register.php'>Go Back</a>";
+
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        echo "<h2>Registration Error</h2>";
+        echo "<p>Please enter a valid email address.</p>";
+        echo "<a class='btn' href='register.php'>Go Back</a>";
 
     } else {
 
         $file = fopen("data_registrations.csv", "a");
-
         $date = date("Y-m-d");
 
         fputcsv($file, array($name, $id, $email, $event, $date));
-
         fclose($file);
 
         echo "<h2>Registration completed successfully.</h2>";
-        echo "<a href='register.php'>Register another student</a><br><br>";
-        echo "<a href='registrations.php'>View Registration List</a>";
-
+        echo "<p><a class='btn' href='register.php'>Register Another Student</a></p>";
+        echo "<p><a class='btn' href='registrations.php'>View Registration List</a></p>";
     }
 
 } else {
 
-    echo "Invalid request.";
-
+    echo "<h2>Invalid Request</h2>";
+    echo "<a class='btn' href='register.php'>Return to Registration</a>";
 }
 
 ?>
+
+    </div>
+</main>
+
+<?php include "footer.php"; ?>
